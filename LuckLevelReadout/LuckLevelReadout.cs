cdda4b30d794dd2686a131fe96b3c3b0db6bd8fb
tytyereal <58371547+tytyereal@users.noreply.github.com>
2022-05-26 17:36:42 -0400

    Add project files.

---
+++ LuckLevelReadout/LuckLevelReadout.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using TMPro;
+
+namespace LuckLevelReadout
+{
+    public class LuckLevelReadout : MelonMod
+    {
+        [HarmonyPatch(typeof(Player), "Update")]
+        class LuckUpdatePatch
+        {
+            static void Postfix(ref Player __instance, ref TextMeshProUGUI ___experienceText)
+            {
+                ___experienceText.text =
+                "Experience: " + __instance.experience.ToString() + " / " + __instance.experienceToLevels[__instance.level].ToString() +
+                " uncommonLuck: " + __instance.uncommonLuck.ToString() +
+                " rareLuck: " + __instance.rareLuck.ToString() +
+                " legendaryLuck: " + __instance.legendaryLuck.ToString();
+            }
+        }
+    }
+}
