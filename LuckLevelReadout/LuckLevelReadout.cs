68a6cc7ce200721bbe61f673ea02b0f7dc7e4476
tytyereal <58371547+tytyereal@users.noreply.github.com>
2022-06-28 13:15:34 -0400

    Updated to add compatablity with newer game versions

--- LuckLevelReadout/LuckLevelReadout.cs
+++ LuckLevelReadout/LuckLevelReadout.cs
@@ -11,7 +11,7 @@ namespace LuckLevelReadout
         {
             static void Postfix(ref Player __instance, ref TextMeshProUGUI ___experienceText)
             {
-                if(___experienceText.text.ToLower().Equals("max level"))
+                if(__instance.level == __instance.chosenCharacter.levelUps.Count - 1)
                 {
                     ___experienceText.text =
                     "Max Level" +
@@ -21,7 +21,7 @@ namespace LuckLevelReadout
                     return;
                 }
                 ___experienceText.text =
-                "Experience: " + __instance.experience.ToString() + " / " + __instance.experienceToLevels[__instance.level].ToString() +
+                "Experience: " + __instance.experience.ToString() + " / " + __instance.chosenCharacter.levelUps[__instance.level+1].xpRequired +
                 " uncommonLuck: " + __instance.uncommonLuck.ToString() +
                 " rareLuck: " + __instance.rareLuck.ToString() +
                 " legendaryLuck: " + __instance.legendaryLuck.ToString();
